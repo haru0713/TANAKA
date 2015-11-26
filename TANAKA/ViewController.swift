1fc9c4107d38a696584231a3ea201d3b694c4a25
阿部遥人 <abeharuto@abeharukanin-no-macbook-air.local>
2015-11-26 19:45:51 +0900

    Initial Commit

---
+++ TANAKA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TANAKA
+//
+//  Created by 阿部遥人 on 2015/11/26.
+//  Copyright © 2015年 阿部遥人. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
